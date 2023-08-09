7c716ae255b853486539189adf95664975039119
Bankbtv07 <btvbank07@gmail.com>
2023-08-09 10:37:00 +0700

    config

--- src/main/java/La/OpenTecLab/Training/backend/service/BackendService.java
+++ src/main/java/La/OpenTecLab/Training/backend/service/BackendService.java
@@ -10,7 +10,6 @@ import La.OpenTecLab.Training.backend.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
